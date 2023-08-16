e999220b8dca0a6f2f29e47ae52b52764b77e1de
Santiago <sansfleitas@gmail.com>
2023-08-15 22:37:17 -0300

    Create simple routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
-import http from 'node:http';
+import http from "node:http";
 
 const server = http.createServer((req, res) => {
-  return res.end('Hello world');
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res.end("Listagem de usuários");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return res.end("Criação de usuário");
+  }
+
+  return res.end("Hello Ignite");
 });
 
 server.listen(3333);
